2b224c0205deb3dd08a87022c0de7b8f51722921
Candice8976 <candicebouquin@MacBook-Pro-de-Candice.local>
2024-11-04 10:19:42 +0100

    ajout fichiers

--- code/preprocess/cleaning_data.py
+++ code/preprocess/cleaning_data.py
@@ -1,10 +1,12 @@
+import pandas as pd
+
 def outliers(dataframe, column,treshold_sup=None, treshold_inf=None, get_info=False):
     """
     function that returns a list of all outliers in a column depending on the treshold
     
     Args:
         dataframe : 
-        column : name of the column
+        column (string) : name of the column
         treshold_sup (int,float, optional): treshold for the outliers superior to a value. Defaults to None.
         treshold_inf (int,float, optional): treshold for the outliers inferior to a value. Defaults to None.
         get_info (bool, optional): _description_. Defaults to False.
@@ -12,7 +14,7 @@ def outliers(dataframe, column,treshold_sup=None, treshold_inf=None, get_info=Fa
     Returns:
         outliers: list with all outliers 
     """
-    
+
     #verification that treshold_sup is a value
     if treshold_sup is not None:
         if not isinstance(treshold_sup, (int, float)):
@@ -23,16 +25,25 @@ def outliers(dataframe, column,treshold_sup=None, treshold_inf=None, get_info=Fa
         if not isinstance(treshold_inf, (int, float)):
             raise ValueError('treshold_inf must be an int or float')
     
-    outliers_sup=[]
-    outliers_inf=[]
+    if get_info : 
+        outliers=pd.DataFrame()
+        for i in range(len(dataframe[column])):
+            if treshold_sup is not None :
+                if dataframe[column][i]>treshold_sup: outliers.append(dataframe[column][i].item())
+            if treshold_inf is not None :
+                if dataframe[column][i]<treshold_inf: outliers.append(dataframe[column][i].item())
+        return 
 
-    for i in range(len(column)):
-        if treshold_sup is not None :
-            if column[i]>treshold_sup: outliers_sup.append(column[i].item())
-        if treshold_inf is not None :
-            if column[i]<treshold_inf: outliers_inf.append(column[i].item())
-
-    if len(outliers_sup)>0 and len(outliers_inf)>0: return outliers_sup,outliers_inf
-    elif len(outliers_sup)>0 : return outliers_sup
-    elif len(outliers_inf)>0 : return outliers_inf
-    else : print('There are no outliers for this column and this treshold')    
+    else :
+        outliers_sup=[]
+        outliers_inf=[]
+        for i in range(len(dataframe[column])):
+            if treshold_sup is not None :
+                if dataframe[column][i]>treshold_sup: outliers_sup.append(dataframe[column][i].item())
+            if treshold_inf is not None :
+                if dataframe[column][i]<treshold_inf: outliers_inf.append(dataframe[column][i].item())
+    
+        if len(outliers_sup)>0 and len(outliers_inf)>0: return outliers_sup,outliers_inf
+        elif len(outliers_sup)>0 : return outliers_sup
+        elif len(outliers_inf)>0 : return outliers_inf
+        else : print('There are no outliers for this column and this treshold')    
