fa38b1695fce335a295daf0cea137cbd6a0ea01e
sarahgrc <sarah.garcia@epfedu.fr>
2024-12-02 17:10:27 +0100

    refractor

---
+++ app_streamlit/analyse/visualisation_recettes.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def visualisation_seasons(df):
+    """
+    Visualisation of best time to publish
+    :param df:
+    :return:
+    """
+
--- code/main.py
+++
@@ -1,6 +0,0 @@
-from preprocess.clean_dataframe import *
-
-#create the clean dataframe 
-clean_df=prepare_final_dataframe(pd.read_csv('data_files/RAW_interactions.csv'),pd.read_csv('data_files/RAW_recipes.csv'),pd.read_csv('data_files/PP_recipes.csv'))
-
-
