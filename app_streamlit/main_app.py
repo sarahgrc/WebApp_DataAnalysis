60f80e5994598a6d403996fa104ffa9e3755e6ea
sarahgrc <114236714+sarahgrc@users.noreply.github.com>
2024-12-09 00:08:06 +0100

    Update main_app.py

--- app_streamlit/main_app.py
+++ app_streamlit/main_app.py
@@ -9,39 +9,6 @@ import os
 import zipfile
 import gdown
 
-# download all the necessary files for the project 
-
-zip_url = 'https://drive.google.com/file/d/11KFS8Kiyivn0vvaOJwHiNo42CAduzLuV/view?usp=drive_link'
-file_id = zip_url.split('/d/')[1].split('/')[0]
-download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
-folder_storage = '../data_files'
-
-def download_extract_zip(gdrive_url, out_dir):
-    """
-    Download a Google Drive Zip archive and extract it to wanted folder
-
-    Args:
-        gdrive_url (str): Google Drive URL of the download zip archive.
-        out_dir (str): Path where to extract the zip files.
-    """
-    # Create output path if not existing
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-
-    try:
-        # Download zip file
-        local_zip_path = os.path.join(out_dir, "data.zip")
-        gdown.download(gdrive_url, local_zip_path, quiet=False)
-
-        # Extract zip file
-        with zipfile.ZipFile(local_zip_path, 'r') as zipf:
-            zipf.extractall(out_dir)
-            print(' -- Extraction completed --')
-        
-        # Remove zip file
-        os.remove(local_zip_path)
-    except Exception as e:
-        print("*** ERROR ***", e)
 
 # Wrapper functions for pages
 def display_recipes_page_wrapper():
